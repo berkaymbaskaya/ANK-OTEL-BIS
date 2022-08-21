62ef695ca7dd6f70aeca0f613d0dc991c6b951a6
berkaymbaskaya <110475301+berkaymbaskaya@users.noreply.github.com>
2022-08-21 23:50:49 +0300

    Update ankara.py

--- ankara.py
+++ ankara.py
@@ -137,8 +137,7 @@ else:
 #st.sidebar.image(image)
 folium.LayerControl().add_to(map)
 
-image = st.image("https://r.resimlink.com/0SYCasqXJ-ki.png")
-st.sidebar.image(image)
+st.sidebar.image("https://r.resimlink.com/0SYCasqXJ-ki.png")
 
 folium_static(map)
 Df
